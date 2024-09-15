67c27dad44708090bb632e2bfe99abfe4811f6b0
Nadia Salazar <109547635+Nadia056@users.noreply.github.com>
2024-09-15 04:37:28 -0600

    bugs

--- laravel/app/Http/Controllers/mongoController.php
+++ laravel/app/Http/Controllers/mongoController.php
@@ -10,17 +10,18 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 
+
 class mongoController extends Controller
 {
-    // Probar la conexión con MongoDB
     public function mongoConection()
+
     {
-        $collection = DB::connection('mongodb')->collection('actor')->get();
+
+        $collection = DB::connection('mongodb')->table('actor')->get();
         return $collection;
     }
-
-    // Seleccionar proyecto
-    public function seleccionarProyecto(Request $request)
+    //estudiante
+    public function sleccionarProyecto(Request $request)
     {
         try {
             $validator = Validator::make(
@@ -29,207 +30,275 @@ class mongoController extends Controller
                     'estudiante_id' => 'required',
                     'proyecto_id' => 'required',
                     'URL_propuesta' => 'required',
+                    
                 ],
                 [
                     'estudiante_id.required' => 'El campo estudiante_id es requerido',
                     'proyecto_id.required' => 'El campo proyecto_id es requerido',
                     'URL_propuesta.required' => 'El campo URL_propuesta es requerido',
+                   
                 ]
             );
-
             if ($validator->fails()) {
-                return response()->json([
+                return response()->json(
+                    [
+                        'status' => 400,
+                        'data' => [],
+                        'msg' => 'Error de validación',
+                        'error' => $validator->errors()
+                    ],
+                    400
+                );
+            }
+            $fecha_actual = Carbon::now();
+            //si la fecha de envio es mayor a la fecha actual
+            $proyecto = Proyecto::find($request->proyecto_id);
+            if ($fecha_actual > $proyecto->fecha_limite) {
+                return response()->json(
+                    [
+                        'status' => 400,
+                        'data' => [],
+                        'msg' => 'La etapa de propuesta ha finalizado',
+                    ],
+                    400
+                );
+            }
+            
+            $collection = DB::connection('mongodb')->table('Propuestas')->insert(
+                [
+                    'URL_propuesta' => $request->URL_propuesta,
+                    'fecha_envio' => Carbon::now(),
+                    'estudiante_id' => $request->estudiante_id,
+                    'proyecto_id' => $request->proyecto_id,
+                    'etapa' => 0,
+                    'status' => 1,
+                ]
+            );
+            return response()->json(
+                [
+                    'status' => 200,
+                    'data' => $collection,
+                    'msg' => 'Propuesta creada correctamente',
+                ],
+                200
+            );
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return response()->json(
+                [
                     'status' => 400,
                     'data' => [],
                     'msg' => 'Error de validación',
-                    'error' => $validator->errors()
-                ], 400);
-            }
 
-            $fecha_actual = Carbon::now();
-            $proyecto = Proyecto::find($request->proyecto_id);
-
-            if (!$proyecto) {
-                return response()->json([
-                    'status' => 400,
-                    'msg' => 'Proyecto no encontrado',
-                ], 400);
-            }
-
-            if ($fecha_actual > $proyecto->fecha_limite) {
-                return response()->json([
-                    'status' => 400,
-                    'msg' => 'La etapa de propuesta ha finalizado',
-                ], 400);
-            }
-
-            $collection = DB::connection('mongodb')->collection('Propuestas')->insert([
-                'URL_propuesta' => $request->URL_propuesta,
-                'fecha_envio' => Carbon::now(),
-                'estudiante_id' => $request->estudiante_id,
-                'proyecto_id' => $request->proyecto_id,
-                'etapa' => 0,
-                'status' => 1,
-            ]);
-
-            return response()->json([
-                'status' => 200,
-                'data' => $collection,
-                'msg' => 'Propuesta creada correctamente',
-            ], 200);
-        } catch (\Exception $e) {
-            Log::error('Exception during seleccionarProyecto: ' . $e->getMessage());
-            return response()->json([
-                'status' => 500,
-                'msg' => 'Error al crear la propuesta.',
-                'error' => $e->getMessage()
-            ], 500);
+                ],
+                400
+            );
         }
     }
-
-    // Obtener historial
+    
     public function historial($estudiante_id)
     {
-        try {
-            $collection = DB::connection('mongodb')->collection('Propuestas')
-                ->where('estudiante_id', $estudiante_id)
+        try{
+        //obtener las propuestas a las que el estudiante pertenece
+        $collection = DB::connection('mongodb')->table('Propuestas')
+            ->where('estudiante_id', $estudiante_id)->where('status', 1)->first();
+
+        
+            if (is_null($collection)) {
+            $hoy = Carbon::now('America/Monterrey')->toDateString();
+
+            // Realizar la consulta para obtener los proyectos
+            $proyectos = Proyecto::where('fecha_creacion', '<=', $hoy)
+                ->where('fecha_limite', '>=', $hoy) 
                 ->where('status', 1)
-                ->first();
+                ->get();
 
-            if (!$collection) {
-                $hoy = Carbon::now('America/Monterrey')->toDateString();
-                $proyectos = Proyecto::where('fecha_creacion', '<=', $hoy)
-                    ->where('fecha_limite', '>=', $hoy)
-                    ->where('status', 1)
-                    ->get();
-
-                return response()->json([
+            return response()->json(
+                [
                     'status' => 200,
                     'data' => $proyectos,
                     'msg' => 'Proyectos disponibles',
-                ], 200);
-            }
-
-            $msg = match ($collection->etapa) {
-                0 => 'Propuesta en espera de aceptación',
-                1 => 'Propuesta en espera de vista y/o corrección',
-                2 => 'Propuesta en etapa de desarrollo',
-                3 => 'Propuesta en etapa de finalización',
-                default => 'Etapa desconocida',
-            };
-
-            return response()->json([
-                'status' => 200,
-                'data' => $collection,
-                'msg' => $msg,
-            ], 200);
-        } catch (\Exception $e) {
-            Log::error('Exception during historial: ' . $e->getMessage());
-            return response()->json([
+                    'error' => []
+                ], 200
+            );
+        }
+        elseif($collection->etapa == 0)
+        {
+            return response()->json(
+                [
+                    'status' => 200,
+                    'data' => $collection,
+                    'msg' => 'Propuesta en espera de aceptación',
+                ],
+                200
+            );
+        }
+        elseif($collection->etapa == 1)
+        {
+            return response()->json(
+                [
+                    'status' => 200,
+                    'data' => $collection,
+                    'msg' => 'Propuesta en espera de vista y/o corrección',
+                ],
+                200
+            );
+        }
+        elseif($collection->etapa == 2)
+        {
+            return response()->json(
+                [
+                    'status' => 200,
+                    'data' => $collection,
+                    'msg' => 'Propuesta en etapa de desarrollo',
+                ],
+                200
+            );
+        }
+        elseif($collection->etapa == 3)
+        {
+            return response()->json(
+                [
+                    'status' => 200,
+                    'data' => $collection,
+                    'msg' => 'Propuesta en etapa de finalización',
+                ],
+                200
+            );
+        }
+    } catch (\Exception $e) {
+        Log::error('Exception during historial: ' . $e->getMessage());
+        return response()->json(
+            [
                 'status' => 500,
+                'data' => [],
                 'msg' => 'Error al obtener el historial.',
                 'error' => $e->getMessage()
-            ], 500);
-        }
+            ], 500
+        );
     }
-
-    // Subir vistas
+    }
     public function subirVistas(Request $request)
     {
-        try {
-            $validator = Validator::make(
-                $request->all(),
+        try{
+        $validator=Validator::make(
+            $request->all(),
+            [
+                'id_propuesta' => 'required',
+                'URL_Imagen' => 'required',
+            ],
+            [
+                'id_propuesta.required' => 'El campo id_propuesta es requerido',
+                'URL_Imagen.required' => 'El campo URL_Imagen es requerido',
+            ]
+        );
+        if ($validator->fails()) {
+            return response()->json(
                 [
-                    'id_propuesta' => 'required',
-                    'URL_Imagen' => 'required',
-                ],
-                [
-                    'id_propuesta.required' => 'El campo id_propuesta es requerido',
-                    'URL_Imagen.required' => 'El campo URL_Imagen es requerido',
-                ]
-            );
-
-            if ($validator->fails()) {
-                return response()->json([
                     'status' => 400,
                     'data' => [],
                     'msg' => 'Error de validación',
                     'error' => $validator->errors()
-                ], 400);
-            }
+                ],
+                400
+            );
+        }
 
-            $collection = DB::connection('mongodb')->collection('Vistas')->insert([
+        $collection = DB::connection('mongodb')->table('Vistas')->insert(
+            [
                 'id_propuesta' => $request->id_propuesta,
                 'URL_Imagen' => $request->URL_Imagen,
                 'fecha_envio' => Carbon::now(),
-            ]);
-
-            return response()->json([
+            ]
+        );
+        return response()->json(
+            [
                 'status' => 200,
                 'data' => $collection,
                 'msg' => 'Vista subida correctamente',
-            ], 200);
-        } catch (\Exception $e) {
-            Log::error('Exception during subirVistas: ' . $e->getMessage());
-            return response()->json([
+            ],
+            200
+        );
+    } catch (\Exception $e) {
+        Log::error('Exception during subirVistas: ' . $e->getMessage());
+        return response()->json(
+            [
                 'status' => 500,
+                'data' => [],
                 'msg' => 'Error al subir la vista.',
                 'error' => $e->getMessage()
-            ], 500);
-        }
-    }
+            ], 500
+        );
+    }}
 
-    // Obtener propuestas para un proyecto
+    //admin
     public function getPropuestas($id_proyecto)
     {
         try {
-            $collection = DB::connection('mongodb')->collection('Propuestas')
+            $collection = DB::connection('mongodb')->table('Propuestas')
                 ->where('id_proyecto', $id_proyecto)
                 ->where('etapa', '!=', 0)
                 ->where('status', 1)
                 ->first();
-
-            if (!$collection) {
-                $collection = DB::connection('mongodb')->collection('Propuestas')
+    
+                if (is_null($collection)) {
+                $collection = DB::connection('mongodb')->table('Propuestas')
                     ->where('id_proyecto', $id_proyecto)
                     ->where('etapa', 0)
                     ->where('status', 1)
                     ->get();
-
-                return response()->json([
+                return response()->json(
+                    [
+                        'status' => 200,
+                        'data' => $collection,
+                        'msg' => 'Propuestas encontradas',
+                    ],
+                    200
+                );
+            } elseif ($collection->first()->etapa == 1) {
+                // Retorna la propuesta con la vista
+                $propuesta = $collection->first();
+                $vista = DB::connection('mongodb')->table('Vistas')
+                    ->where('id_propuesta', $propuesta->_id)
+                    ->first();
+    
+                $propuesta->vista = $vista;
+    
+                return response()->json(
+                    [
+                        'status' => 200,
+                        'data' => $propuesta,
+                        'msg' => 'Propuesta con vista encontrada',
+                    ],
+                    200
+                );
+            }
+    
+            return response()->json(
+                [
                     'status' => 200,
                     'data' => $collection,
                     'msg' => 'Propuestas encontradas',
-                ], 200);
-            }
-
-            if ($collection->etapa == 1) {
-                $vista = DB::connection('mongodb')->collection('Vistas')
-                    ->where('id_propuesta', $collection->_id)
-                    ->first();
-                $collection->vista = $vista;
-            }
-
-            return response()->json([
-                'status' => 200,
-                'data' => $collection,
-                'msg' => 'Propuestas encontradas',
-            ], 200);
+                ],
+                200
+            );
         } catch (\Exception $e) {
             Log::error('Exception during getPropuestas: ' . $e->getMessage());
-            return response()->json([
-                'status' => 500,
-                'msg' => 'Error al obtener las propuestas.',
-                'error' => $e->getMessage()
-            ], 500);
+            return response()->json(
+                [
+                    'status' => 500,
+                    'data' => [],
+                    'msg' => 'Error al obtener las propuestas.',
+                    'error' => $e->getMessage()
+                ],
+                500
+            );
         }
     }
-
-    // Actualizar etapas de propuestas
     public function etapasPropuestas(Request $request)
     {
-        try {
-            $validator = Validator::make(
+        try{
+            $validator=Validator::make(
                 $request->all(),
                 [
                     'id_propuesta' => 'required',
@@ -240,58 +309,76 @@ class mongoController extends Controller
                     'etapa.required' => 'El campo etapa es requerido',
                 ]
             );
-
             if ($validator->fails()) {
-                return response()->json([
-                    'status' => 400,
-                    'data' => [],
-                    'msg' => 'Error de validación',
-                    'error' => $validator->errors()
-                ], 400);
+                return response()->json(
+                    [
+                        'status' => 400,
+                        'data' => [],
+                        'msg' => 'Error de validación',
+                        'error' => $validator->errors()
+                    ],
+                    400
+                );
             }
-
-            $collection = DB::connection('mongodb')->collection('Propuestas')
-                ->where('id', $request->id_propuesta)
-                ->update([
-                    'etapa' => $request->etapa,
-                    'fecha_envio' => Carbon::now(),
-                ]);
-
-            return response()->json([
+        //actualiza la etapa de la propuesta a 1
+        $collection = DB::connection('mongodb')
+        ->collection('Propuestas')  
+        ->where('id', $request->id_propuesta) 
+        ->update([
+            'etapa' => $request->etapa,
+            'fecha_envio' => Carbon::now()
+        ]); 
+        return response()->json(
+            [
                 'status' => 200,
                 'data' => $collection,
                 'msg' => 'Etapa actualizada',
-            ], 200);
-        } catch (\Exception $e) {
-            Log::error('Exception during etapasPropuestas: ' . $e->getMessage());
-            return response()->json([
+            ],
+            200
+        );
+    } catch (\Exception $e) {
+        Log::error('Exception during etapasPropuestas: ' . $e->getMessage());
+        return response()->json(
+            [
                 'status' => 500,
+                'data' => [],
                 'msg' => 'Error al actualizar la etapa.',
                 'error' => $e->getMessage()
-            ], 500);
-        }
+            ], 500
+        );
     }
-
-    // Cancelar propuesta
+    }
+    
     public function cancelarPropuesta($id_propuesta)
     {
-        try {
-            $collection = DB::connection('mongodb')->collection('Propuestas')
-                ->where('id', $id_propuesta)
-                ->update(['status' => 0]);
-
-            return response()->json([
+        try{
+        //actualiza la etapa de la propuesta a 1
+        $collection=DB::connection('mongodb')
+            ->collection('Propuestas')  // Usar 'collection' en vez de 'table'
+            ->where('id', $id_propuesta) // Filtrar por 'id'
+            ->update(['status' => 0]);   
+        return response()->json(
+            [
                 'status' => 200,
                 'data' => $collection,
                 'msg' => 'Propuesta cancelada',
-            ], 200);
-        } catch (\Exception $e) {
-            Log::error('Exception during cancelarPropuesta: ' . $e->getMessage());
-            return response()->json([
+            ],
+            200
+        );
+    } catch (\Exception $e) {
+        Log::error('Exception during cancelarPropuesta: ' . $e->getMessage());
+        return response()->json(
+            [
                 'status' => 500,
+                'data' => [],
                 'msg' => 'Error al cancelar la propuesta.',
                 'error' => $e->getMessage()
-            ], 500);
-        }
+            ], 500
+        );
     }
+    }
+    
+   
+    
+
 }
